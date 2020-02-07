fba3198388111de84acbc10b2b63c91663250046
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-07 20:42:16 +0545

    Create 1.c

---
+++ assignment 2/1.c
@@ -0,0 +1,22 @@
+// To conver decimal number into binary:
+
+#include <stdio.h>
+
+int main()
+{
+	int dec, binary[100],i = 0;
+	printf("Enter the decimal number : ");
+	scanf(" %d", &dec);
+	
+	while(dec != 0)
+	{
+		binary[i] = dec%2;
+		dec = dec/2;
+		i++;	
+	}
+	for( i = i-1; i>= 0; i--)
+	{
+		printf("%d", binary[i]);
+	}
+	return 0;
+}
