a2dd670f6fe85e49eb6f4324cecbdcc72ec2c53a
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-07 22:47:30 -0500

    Create 6.c

---
+++ assignment 2/6.c
@@ -0,0 +1,37 @@
+// To print the Fibonacci number up to given limti: 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a =1,b =1, lim;
+	printf("Enter the limit of the series: ");
+	scanf(" %d", &lim);
+	
+	while(1)
+	{
+		if (a <lim)
+		{
+			printf("%d ", a);
+		}
+		else
+		{
+				exit(0);
+		}
+		if (b < lim)
+		{
+			printf("%d ", b);
+		}
+		else
+		{
+			exit(0);
+		}
+		a = a + b;
+		b = b + a;
+	}
+	
+	
+	
+	return 0;
+}
