699272c17c6ca99aba06b346ed0ef9e2c69aa439
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-08 20:41:21 -0500

    Create 12.c

---
+++ assignment 2/12.c
@@ -0,0 +1,24 @@
+// To find the sum of odd and even number in range 1-100:
+
+#include <stdio.h>
+
+int main()
+{
+	int i, odd=0, even=0;
+	
+	for(i = 1; i<=100; i++)
+	{
+		if(i%2 == 0)
+		{
+			even += i;
+		}
+		else
+		{
+			odd += i;
+		}
+	}
+	printf(" sum odd = %d \n sum even = %d", odd, even);
+	
+	return 0;
+}
+
