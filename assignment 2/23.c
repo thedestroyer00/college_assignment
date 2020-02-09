979e7e144dd810607d05f02d4e3732a29378e4bf
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-09 09:33:05 -0500

    Create 23.c

---
+++ assignment 2/23.c
@@ -0,0 +1,26 @@
+// To read salary of 100 employees and print number of employees whose salary is in between 10000 and 20000
+
+#include <stdio.h>
+# define max 100
+
+int main()
+{
+	int salary[max], i,employees =0;
+	printf("Enter the salary of the employess:");
+	for(i = 0; i< max; i++)
+	{
+		scanf(" %d", &salary[i]);
+	}
+	
+	for(i = 0; i<max; i++)
+	{
+		if(salary[i] > 10000 && salary[i] < 20000)
+		{
+			employees++;
+		}
+	}
+	printf("%d employess has salary in between 10000 and 20000",employees);
+	
+	
+	return 0;
+}
