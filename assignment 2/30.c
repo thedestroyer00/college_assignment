e6e6e33a0ef6e5268e176fe4fbe3a70cb0b1bcb9
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-09 10:18:50 -0500

    Create 30.c

---
+++ assignment 2/30.c
@@ -0,0 +1,21 @@
+// To swap two vairiables without using third variable:
+
+# include <stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("Enter any two variable: ");
+	scanf(" %d %d", &a,&b);
+	
+	printf("Befor swap a = %d  b = %d \n", a,b); 
+	
+	a = a+b;
+	b = a - b;
+	a = a-b;
+	
+	printf("After swap a = %d  b = %d",a,b);  
+
+	return 0;
+}
