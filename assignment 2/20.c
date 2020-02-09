effbf0f5da491aa1ab916cbcf59951cf0a3e4415
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-09 08:53:07 -0500

    Create 20.c

---
+++ assignment 2/20.c
@@ -0,0 +1,35 @@
+// To print the given numbers in descending order : 
+
+
+# include <stdio.h>
+# define size 20
+
+int main()
+{
+	int i,j, arr[size];
+	
+	printf("Enter the element of the array: ");
+	for(i = 0; i<size; i++)
+	{
+		scanf(" %d", &arr[i]);
+	}
+	
+	for(i =0; i<size; i++)
+	{
+		for(j = i+1; j < size; j++)
+		{
+			if(arr[i] < arr[j])
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+			
+		}
+	}
+	for(i = 0; i<size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;	
+}
