2fdb5c676ea49dbdc177be5c676ace7b2d8ee567
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-09 08:51:33 -0500

    Update 18.c

--- assignment 2/18.c
+++ assignment 2/18.c
@@ -4,27 +4,14 @@
 
 int main()
 {
-	int i,j, arr[size];
+	int i, arr[size];
 	
 	printf("Enter the element of the array: ");
 	for(i = 0; i<size; i++)
 	{
 		scanf(" %d", &arr[i]);
 	}
-	
-	for(i =0; i<size; i++)
-	{
-		for(j = i+1; j < size; j++)
-		{
-			if(arr[i] > arr[j])
-			{
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-			
-		}
-	}
+
 	for(i = 0; i<size; i++)
 	{
 		printf("%d ", arr[i]);
