50ad6f63275351128161b34e3abbfced4e9c6870
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-02-11 12:13:52 +0545

    Update 16.c

--- assignment 2/16.c
+++ assignment 2/16.c
@@ -1,33 +1,58 @@
 // To find second largest or smallest number among 10 given numbers:
 
 # include <stdio.h>
-# define size 10
+# include <stdlib.h>
+# define max 10
 
 int main()
 {
-	int i,j, arr[size];
+	int arr[max], g,s,i,j, w1 = 1, w2 =1;
 	
-	printf("Enter the element of the array: ");
-	for(i = 0; i<size; i++)
+	printf("Enter the numbers : ");
+	for(i = 0; i<max; i++)
 	{
 		scanf(" %d", &arr[i]);
 	}
 	
-	for(i =0; i<size; i++)
+	for(i = 0; i<max; i++)
 	{
-		for(j = i+1; j < size; j++)
+		for(j = i+1;j <max; j++)
 		{
-			if(arr[i] > arr[j])
+			if(arr[i]>arr[j])
 			{
 				int temp = arr[i];
-				arr[i] = arr[j];
+				arr[i]= arr[j];
 				arr[j] = temp;
 			}
-			
 		}
 	}
-	printf("The second largest number is : %d\n", arr[size - 2]);
-	printf("The smallest number is : %d \n", arr[1]);
+	g = arr[max-1];
+	s = arr[0];
+	i = 1;
+	while(w1 == 1)
+	{
+		if(s == arr[i])
+		{i++;}
+		else
+		{
+			s = arr[i];
+			break;
+		}
+		 
+	}
+	printf("%d is second smallest number.\n", s);
+	j = max-1;
+	while(1)
+	{
+		if(g == arr[j])
+		{j--;}
+		else
+		{
+			g = arr[j]; 
+			break;
+		}
+	}
+	printf("%d is second greatest number.",g );
 	
-	return 0;	
+	return 0;
 }
